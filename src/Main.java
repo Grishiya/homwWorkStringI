32f1e6601774ea36492827da278e5fd97002d571
Grisha <guruwolf8@gmail.com>
2023-06-09 19:00:03 +0300

    Задача 3.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
 
 
     }
@@ -22,6 +23,13 @@ public class Main {
         String middleName = "Ivanovich ";
         String lastName = "Ivanov ";
         String fullName = lastName + firstName + middleName;
-        System.out.println("ФИО сотрудника - " + fullName.toUpperCase());
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3.");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — "+fullName);
     }
 }
